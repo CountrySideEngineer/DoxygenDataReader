baff70f92aef541f2a1de4e5b255c370a7fe1cd8
CountrySideEngineer <deep-sleeper@nifty.com>
2024-01-21 22:49:56 +0900

    Update Program. Update the file to read from and show values in path table of sqlite data base.

--- dev/src/ReadDoxyDatabase/ReadDoxyDatabase/Program.cs
+++ dev/src/ReadDoxyDatabase/ReadDoxyDatabase/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Doxygen.DB;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Contracts;
+
 Console.WriteLine("Hello, World!");
+
+using (var dbContext = new DoxygenDbContext())
+{
+    dbContext.Database.EnsureCreated();
+
+    var entities = dbContext.PathModels;
+
+    foreach (var entity in entities)
+    {
+        Console.WriteLine($"rowid = {entity.RowId, 4:G}, " +
+            $"type = {entity.Type, 4:G}, " +
+            $"local = {entity.Local,4:G}, " +
+            $"found = {entity.Found,4:G}, " +
+            $"name = {entity.Name}"
+            );
+    }
+}
