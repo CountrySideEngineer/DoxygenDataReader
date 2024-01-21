c86e09c1fd96c6fb9183eb9a9c887095e630bf1f
CountrySideEngineer <deep-sleeper@nifty.com>
2024-01-21 22:48:33 +0900

    Add new classes. Add class to access sqlite data base file generated by doxygen using EntityFramework.

---
+++ dev/src/ReadDoxyDatabase/Doxygen/DB/DoxygenDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Doxygen.DB.Table;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doxygen.DB
+{
+    public class DoxygenDbContext : DbContext
+    {
+        public DbSet<PathModel> PathModels { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=.\\doxygen_sqlite3.db");
+        }
+    }
+}
---
+++ dev/src/ReadDoxyDatabase/Doxygen/DB/Table/PathModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doxygen.DB.Table
+{
+    [Table("path")]
+    public class PathModel
+    {
+        [Key]
+        [Column("rowid")]
+        public int RowId { get; set; }
+
+        [Column("type")]
+        public int Type { get; set; }
+
+        [Column("local")]
+        public int Local { get; set; }
+
+        [Column("found")]
+        public int Found { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
