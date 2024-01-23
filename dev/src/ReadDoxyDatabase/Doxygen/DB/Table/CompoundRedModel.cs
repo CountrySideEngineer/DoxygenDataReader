71c7923cc9ed981b53b625506ad0a90b999890ae
CountrySideEngineer <deep-sleeper@nifty.com>
2024-01-23 22:21:51 +0900

    Add model class about compoundref table of database Doxygen generated.

---
+++ dev/src/ReadDoxyDatabase/Doxygen/DB/Table/CompoundRedModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doxygen.DB.Table
+{
+    [Table("compoundref")]
+    public class CompoundRedModel
+    {
+        [Key]
+        [Column("rowid")]
+        public int Rowid { get; set; }
+
+        [Column("base_rowid")]
+        public int BaseRowId { get; set; }
+
+        [Column("derived_rowid")]
+        public int DerivedRowId { get; set; }
+
+        [Column("prot")]
+        public int Prot { get; set; }
+
+        [Column("virt")]
+        public int Virt { get; set; }
+    }
+}
