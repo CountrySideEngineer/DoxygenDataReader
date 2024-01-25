049a6135f056d5f3a1a7299d06dc4c39bf231690
CountrySideEngineer <deep-sleeper@nifty.com>
2024-01-26 07:38:59 +0900

    Add RefIdModel class.

---
+++ dev/src/ReadDoxyDatabase/Doxygen/DB/Table/RefIdModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doxygen.DB.Table
+{
+    [Table("refid")]
+    internal class RefIdModel
+    {
+        [Key]
+        [Column("rowid")]
+        public int RowId { get; set; }
+
+        [Column("refid")]
+        public string RefId { get; set; } = string.Empty;
+    }
+}
