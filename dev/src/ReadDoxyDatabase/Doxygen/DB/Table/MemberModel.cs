023d73ac0588d69bf96134e08be1517031fdeeeb
CountrySideEngineer <deep-sleeper@nifty.com>
2024-01-26 07:52:33 +0900

    Add MemberModel class.

---
+++ dev/src/ReadDoxyDatabase/Doxygen/DB/Table/MemberModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doxygen.DB.Table
+{
+    [Table("member")]
+    public class MemberModel
+    {
+        [Key]
+        [Column("RowId")]
+        public int RowId { get; set; }
+
+        [Column("scope_rowid")]
+        public int ScopeRowId { get; set; }
+
+        [Column("memeberdef_rowdi")]
+        public int MemberDefRowId { get; set; }
+
+        [Column("prot")]
+        public int Prot { get; set; }
+
+        [Column("virt")]
+        public int Virt { get; set; }
+    }
+}
