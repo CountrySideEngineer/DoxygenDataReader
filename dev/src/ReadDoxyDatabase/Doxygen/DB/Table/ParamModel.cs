9f15cbf3dd30a3a0a769317fb8d4e39fd455b8d3
CountrySideEngineer <deep-sleeper@nifty.com>
2024-01-26 08:00:43 +0900

    Add ParamModel class.

---
+++ dev/src/ReadDoxyDatabase/Doxygen/DB/Table/ParamModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doxygen.DB.Table
+{
+    [Table("param")]
+    public class ParamModel
+    {
+        [Key]
+        [Column("rowid")]
+        public int RowId { get; set; }
+
+        [Column("attributes")]
+        public string Attributes { get; set; }
+
+        [Column("type")]
+        public string Type { get; set; }
+
+        [Column("declname")]
+        public string DeclName { get; set; }
+
+        [Column("defname")]
+        public string DefName { get; set; }
+
+        [Column("array")]
+        public string Array { get; set; }
+
+        [Column("defval")]
+        public string DefVal { get; set; }
+
+        public string BriefDescription { get; set; }
+    }
+}
