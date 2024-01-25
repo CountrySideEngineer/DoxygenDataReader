48e60b74cad628536a19b97f57ed12e5c65c5ea2
CountrySideEngineer <deep-sleeper@nifty.com>
2024-01-26 08:05:28 +0900

    Add IncludesModel class.

---
+++ dev/src/ReadDoxyDatabase/Doxygen/DB/Table/IncludesModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doxygen.DB.Table
+{
+    [Table("includes")]
+    public class IncludesModel
+    {
+        [Key]
+        [Column("rowid")]
+        public int RowId { get; set; }
+
+        [Column("local")]
+        public int Local { get; set; }
+
+        [Column("src_id")]
+        public int SrcId { get; set; }
+
+        [Column("dst_id")]
+        public int DstId { get; set; }
+    }
+}
