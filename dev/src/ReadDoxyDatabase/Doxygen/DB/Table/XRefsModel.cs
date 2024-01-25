4209b174db41beaec8166466a80b006e26a4d3b0
CountrySideEngineer <deep-sleeper@nifty.com>
2024-01-26 07:43:31 +0900

    Add XRefsModel class.

---
+++ dev/src/ReadDoxyDatabase/Doxygen/DB/Table/XRefsModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doxygen.DB.Table
+{
+    [Table("xrefs")]
+    public class XRefsModel
+    {
+        [Key]
+        [Column("rowid")]
+        public int RowId { get; set; }
+
+        [Column("src_rowid")]
+        public int SrcRowId { get; set; }
+
+        [Column("dst_rowid")]
+        public int DstRowId { get; set; }
+
+        [Column("context")]
+        public string Context { get; set; } = string.Empty;
+    }
+}
