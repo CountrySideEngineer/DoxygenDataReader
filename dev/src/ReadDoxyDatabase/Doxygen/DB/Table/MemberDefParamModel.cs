e0efcd687c0b4fc3f9904e00271399b61ff25867
CountrySideEngineer <deep-sleeper@nifty.com>
2024-01-26 07:55:42 +0900

    Add MemberDefParamModel class.

---
+++ dev/src/ReadDoxyDatabase/Doxygen/DB/Table/MemberDefParamModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doxygen.DB.Table
+{
+    [Table("memberdef_param")]
+    public class MemberDefParamModel
+    {
+        [Key]
+        [Column("rowid")]
+        public int RowId { get; set; }
+
+        [Column("memberdef_id")]
+        public int MemberDefId { get; set; }
+
+        [Column("param_id")]
+        public int ParamId { get; set; }
+    }
+}
