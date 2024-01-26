08165425f1cab120827719f13f8441fc47f44230
CountrySideEngineer <deep-sleeper@nifty.com>
2024-01-27 08:10:40 +0900

    Add "Required" attribute to some properties in CompoundDefModel class.

--- dev/src/ReadDoxyDatabase/Doxygen/DB/Table/CompoundDefModel.cs
+++ dev/src/ReadDoxyDatabase/Doxygen/DB/Table/CompoundDefModel.cs
@@ -15,24 +15,29 @@ namespace Doxygen.DB.Table
         [Column("rowid")]
         public int RowId { get; set; }
 
+        [Required]
         [Column("name")]
         public string Name { get; set; } = string.Empty;
 
         [Column("type")]
         public string Title { get; set; } = string.Empty;
 
+        [Required]
         [Column("kind")]
         public string Kind { get; set; } = string.Empty;
 
         [Column("prot")]
         public int Prot { get; set; } = 0;
 
+        [Required]
         [Column("field_id")]
         public int FiedId { get; set; } = 0;
 
+        [Required]
         [Column("line")]
         public int Line { get; set; } = 0;
 
+        [Required]
         [Column("column")]
         public int Column { get; set; } = 0;
 
