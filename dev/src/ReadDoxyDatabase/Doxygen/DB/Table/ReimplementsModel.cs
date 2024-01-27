d8ddcd5724bc1a30236895b82b9d45ea82eeea5b
CountrySideEngineer <deep-sleeper@nifty.com>
2024-01-28 07:19:30 +0900

    Add ReimplementedRowId class.

---
+++ dev/src/ReadDoxyDatabase/Doxygen/DB/Table/ReimplementsModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doxygen.DB.Table
+{
+    [Table("reimplements")]
+    public class ReimplementsModel
+    {
+        [Key]
+        [Column("rowid")]
+        public int RowId { get; set; }
+
+        [Required]
+        [Column("memberdef_rowid")]
+        public int MemberDefRowId { get; set; }
+
+        [Required]
+        [Column("reimplemented_rowid")]
+        public int ReimplementedRowId { get; set; }
+    }
+}
