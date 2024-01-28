efb5396842bab9e3907151b9da11dbdd7ae53daf
CountrySideEngineer <deep-sleeper@nifty.com>
2024-01-28 15:51:23 +0900

    Add dao class to get source file from data base.

---
+++ dev/src/ReadDoxyDatabase/Doxygen/DAO/CppSourceFileDao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doxygen.DAO
+{
+    public class CppSourceFileDao : AFileDao
+    {
+        protected override string GetFileExtension()
+        {
+            return "cpp";
+        }
+    }
+}
