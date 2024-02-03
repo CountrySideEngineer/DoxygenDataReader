4fcf2a3a74bc7e9274306adca31a36c74ac91ffb
CountrySideEngineer <deep-sleeper@nifty.com>
2024-02-04 05:40:31 +0900

    Add ParamDto class.

---
+++ dev/src/ReadDoxyDatabase/Doxygen/DTO/ParamDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doxygen.DTO
+{
+    public class ParamDto : ParamDtoBase
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public ParamDto() : base() { }
+    }
+}
