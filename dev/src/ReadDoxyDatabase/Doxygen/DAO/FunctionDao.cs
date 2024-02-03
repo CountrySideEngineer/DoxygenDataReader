f30b594c1f9fc23e81b3d3e6da3dd8dbd9a01878
CountrySideEngineer <deep-sleeper@nifty.com>
2024-02-03 11:44:30 +0900

    Add FunctionDao class.

---
+++ dev/src/ReadDoxyDatabase/Doxygen/DAO/FunctionDao.cs
@@ -0,0 +1,59 @@
+﻿using Doxygen.DB;
+using Doxygen.DB.Table;
+using Doxygen.DTO;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doxygen.DAO
+{
+    public class FunctionDao
+    {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public FunctionDao() { }
+
+        public IEnumerable<FunctionDto> GetAll()
+        {
+            using (var context = new DoxygenDbContext())
+            {
+                context.Database.EnsureCreated();
+
+                var memberDefParamModels = context.MemberDefParamModels;
+                var memberDefModels = context.MemberDefModels;
+
+                var functions = memberDefParamModels.Join(
+                    memberDefModels,
+                    memberDefParamModel => memberDefParamModel.MemberDefId,
+                    memberDefModel => memberDefModel.RowId,
+                    (memberDefParam, memberDefModel) => new
+                    {
+                        Id = memberDefParam.RowId,
+                        Name = memberDefModel.Name,
+                        Type = memberDefModel.Type,
+                        Kind = memberDefModel.Kind,
+                    }
+                    )
+                    .Where(_ => _.Kind.Equals("function"));
+
+                var dtos = new List<FunctionDto>();
+                foreach (var item in functions)
+                {
+                    var dto = new FunctionDto()
+                    {
+                        Id = item.Id,
+                        Name = item.Name,
+                        Type = item.Type
+                    };
+                    dtos.Add(dto);
+                }
+
+                return dtos;
+            }
+        }
+    }
+}
