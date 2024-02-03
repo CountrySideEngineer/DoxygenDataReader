87537d1d93d9f4e37717d3e463c8ed7515d970fa
CountrySideEngineer <deep-sleeper@nifty.com>
2024-02-04 06:53:33 +0900

    Add ParamDao class.

---
+++ dev/src/ReadDoxyDatabase/Doxygen/DAO/ParamDao.cs
@@ -0,0 +1,48 @@
+﻿using Doxygen.DB;
+using Doxygen.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doxygen.DAO
+{
+    public class ParamDao
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public ParamDao() { }
+
+        public IEnumerable<ParamDto> GetAll()
+        {
+            using (var context = new DoxygenDbContext())
+            {
+                context.Database.EnsureCreated();
+
+                var memberDefParamModels = context.MemberDefParamModels;
+                var paramModels = context.ParamModels;
+                var parameters = paramModels.ToList();
+
+                var dtos = new List<ParamDto>();
+                foreach (var item in parameters)
+                {
+                    var dto = new ParamDto()
+                    {
+                        Id = item.RowId,
+                        Attribute = item.Attributes,
+                        Type = item.Type,
+                        Name = item.DeclName,
+                        DefaultName = item.DefName,
+                        IsArray = Convert.ToInt32(item.Array),
+                        DefaultValue = item.DefVal,
+                        BriefDescription = item.BriefDescription
+                    };
+                    dtos.Add(dto);
+                }
+                return dtos;
+            }
+        }
+    }
+}
