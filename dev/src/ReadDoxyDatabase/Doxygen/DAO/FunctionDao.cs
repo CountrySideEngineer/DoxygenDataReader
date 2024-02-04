e2a18acae05d4366db7a1f968d1322646d24e317
CountrySideEngineer <deep-sleeper@nifty.com>
2024-02-04 10:14:17 +0900

    Update FunctionDao class. Add method to get arguments a function has from data base. Add code to call the added method and set the that of result to FunctionDto object.

--- dev/src/ReadDoxyDatabase/Doxygen/DAO/FunctionDao.cs
+++ dev/src/ReadDoxyDatabase/Doxygen/DAO/FunctionDao.cs
@@ -17,6 +17,49 @@ namespace Doxygen.DAO
         /// </summary>
         public FunctionDao() { }
 
+        protected IEnumerable<ParamDto> GetArgumentsByIdOfFunc(int memberDefId, DoxygenDbContext context)
+        {
+            var memberDerParamModels = context.MemberDefParamModels;
+            var paramModels = context.ParamModels;
+
+            var argumentsModels = memberDerParamModels.Join(
+                paramModels,
+                memberDefParamModel => memberDefParamModel.ParamId,
+                paramModel => paramModel.RowId,
+                (memberDefParamModel, paramModel) => new
+                {
+                    Id = memberDefParamModel.RowId,
+                    memberDefParamModel.MemberDefId,
+                    memberDefParamModel.ParamId,
+                    paramModel.Attributes,
+                    paramModel.Type,
+                    Name = paramModel.DeclName,
+                    DefaultName = paramModel.DefName,
+                    paramModel.Array,
+                    DefaultValue = paramModel.DefVal,
+                    paramModel.BriefDescription
+                })
+                .Where(_ => _.MemberDefId == memberDefId);
+
+            var arguments = new List<ParamDto>();
+            foreach (var item in argumentsModels)
+            {
+                var dto = new ParamDto()
+                {
+                    Id = item.ParamId,
+                    Type = item.Type,
+                    Name = item.Name,
+                    DefaultName = item.DefaultName,
+                    Array = item.Array,
+                    DefaultValue = item.DefaultValue,
+                    BriefDescription = item.BriefDescription
+                };
+                arguments.Add(dto);
+            }
+
+            return arguments;
+        }
+
         public IEnumerable<FunctionDto> GetAll()
         {
             using (var context = new DoxygenDbContext())
@@ -48,12 +91,15 @@ namespace Doxygen.DAO
                 var dtos = new List<FunctionDto>();
                 foreach (var item in functions)
                 {
+                    var arguments = GetArgumentsByIdOfFunc(item.MemberId, context);
+
                     var dto = new FunctionDto()
                     {
                         Id = item.Id,
                         Name = item.Name,
                         Type = item.Type,
-                        Definition = item.Definition
+                        Definition = item.Definition,
+                        Arguments = arguments
                     };
                     dtos.Add(dto);
                 }
