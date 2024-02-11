393dbe9450dac445c7bdb221df60228b81dc10d4
CountrySideEngineer <deep-sleeper@nifty.com>
2024-02-12 06:11:55 +0900

    Update class structure. Add a new abstract class named ADao class, a base class of DAO class. The class has a abstract method and interface of DAO. Update AFileDao class to inherit ADao abstract class. Update FileDto class to inherit ParamDtoBase class. Update code in main function of this application to handle changed above.

--- dev/src/ReadDoxyDatabase/ReadDoxyDatabase/Program.cs
+++ dev/src/ReadDoxyDatabase/ReadDoxyDatabase/Program.cs
@@ -7,12 +7,14 @@ using System.IO;
 using System.Diagnostics.Contracts;
 using System.IO.Enumeration;
 using Doxygen.DAO;
+using Doxygen.DTO;
 
 AFileDao dao = new CppSourceFileDao();
 var files = dao.GetAll();
 
-foreach (var fileItem in files)
+foreach (var item in files)
 {
+    var fileItem = (FileDto)item;
     Console.WriteLine($"ID = {fileItem.Id, 4} " +
         $"NAME:{fileItem.Name, -24}" +
         $"PATH:{fileItem.Path}");
