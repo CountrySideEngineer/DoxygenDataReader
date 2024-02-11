c6cd663f75891f2a30e5148c73d1ddc38c9bdbd1
CountrySideEngineer <deep-sleeper@nifty.com>
2024-02-12 06:21:31 +0900

    Update ParamDao class. Update the class to inherit ADao class and override method the base abstract class.

--- dev/src/ReadDoxyDatabase/Doxygen/DAO/ParamDao.cs
+++ dev/src/ReadDoxyDatabase/Doxygen/DAO/ParamDao.cs
@@ -8,41 +8,38 @@ using System.Threading.Tasks;
 
 namespace Doxygen.DAO
 {
-    public class ParamDao
+    public class ParamDao : ADao
     {
         /// <summary>
         /// Default constructor.
         /// </summary>
-        public ParamDao() { }
+        public ParamDao() : base() { }
 
-        public virtual IEnumerable<ParamDto> GetAll()
+        public override IEnumerable<ParamDtoBase> GetAll(DoxygenDbContext context)
         {
-            using (var context = new DoxygenDbContext())
+            context.Database.EnsureCreated();
+
+            var memberDefParamModels = context.MemberDefParamModels;
+            var paramModels = context.ParamModels;
+            var parameters = paramModels.ToList();
+
+            var dtos = new List<ParamDto>();
+            foreach (var item in parameters)
             {
-                context.Database.EnsureCreated();
-
-                var memberDefParamModels = context.MemberDefParamModels;
-                var paramModels = context.ParamModels;
-                var parameters = paramModels.ToList();
-
-                var dtos = new List<ParamDto>();
-                foreach (var item in parameters)
+                var dto = new ParamDto()
                 {
-                    var dto = new ParamDto()
-                    {
-                        Id = item.RowId,
-                        Attribute = item.Attributes,
-                        Type = item.Type,
-                        Name = item.DeclName,
-                        DefaultName = item.DefName,
-                        IsArray = Convert.ToInt32(item.Array),
-                        DefaultValue = item.DefVal,
-                        BriefDescription = item.BriefDescription
-                    };
-                    dtos.Add(dto);
-                }
-                return dtos;
+                    Id = item.RowId,
+                    Attribute = item.Attributes,
+                    Type = item.Type,
+                    Name = item.DeclName,
+                    DefaultName = item.DefName,
+                    IsArray = Convert.ToInt32(item.Array),
+                    DefaultValue = item.DefVal,
+                    BriefDescription = item.BriefDescription
+                };
+                dtos.Add(dto);
             }
+            return dtos;
         }
     }
 }
