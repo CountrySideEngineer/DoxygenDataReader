8ff7b95fe2b167af5f00e82c764a9d630b24b732
CountrySideEngineer <deep-sleeper@nifty.com>
2024-02-19 21:28:17 +0900

    Add FunctionByFileDao class. FunctionByFileDao class is to get a function by the file id that of the method is implemented.

---
+++ dev/src/ReadDoxyDatabase/Doxygen/DAO/FunctionByFileDao.cs
@@ -0,0 +1,36 @@
+﻿using Doxygen.DTO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doxygen.DAO
+{
+    public class FunctionByFileDao : FunctionDao
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public FunctionByFileDao() : base() { }
+
+        protected virtual IEnumerable<dynamic> GetFunction(int fileId, DbContext context)
+        {
+            IEnumerable<dynamic> functions = base.GetFunction(context).Where(_ => _.BodyFileId.Equals(fileId));
+
+            return functions;
+        }
+
+        public override IEnumerable<ParamDtoBase> GetById(int fileId, DbContext context)
+        {
+            IEnumerable<dynamic> functions = GetFunction(fileId, context);
+            IEnumerable<ParamDtoBase> dtos = ConvertToDto(functions);
+
+            SetupParameters(ref dtos, context);
+
+            return dtos;
+        }
+    }
+}
