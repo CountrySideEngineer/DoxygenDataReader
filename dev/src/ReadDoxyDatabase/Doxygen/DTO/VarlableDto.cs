b9e6c5dc8b5fe0190427fff4b162f3dec5e89e9f
CountrySideEngineer <deep-sleeper@nifty.com>
2024-02-23 07:49:46 +0900

    Add DTO class about variable, global or member variable.

---
+++ dev/src/ReadDoxyDatabase/Doxygen/DTO/VarlableDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doxygen.DTO
+{
+    public class VarlableDto : ParamDtoBase
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public VarlableDto() : base() { }
+
+        /// <summary>
+        /// Global variable definition.
+        /// </summary>
+        public string Definition { get; set; }
+
+        /// <summary>
+        /// Scope of the varlable.
+        /// </summary>
+        public string? Scope { get; set; }
+
+        /// <summary>
+        /// String about variable definition as array.
+        /// </summary>
+        public string? ArrayString { get; set; }
+
+        /// <summary>
+        /// Codes to initialize the variable.
+        /// </summary>
+        public string? Initialize { get; set; }
+    }
+}
